45d8397132b8bda768f1c4d9cec2adce4d2002be
A_Artemyev <anton.artemyev@hb.by>
2020-08-24 09:37:54 +0300

    init

---
+++ catalog/model/extension/payment/card_expresspay.php
@@ -0,0 +1,21 @@
+<?php
+class ModelExtensionPaymentCardExpressPay extends Model {
+	public function getMethod($address, $total) {
+		$this->load->language('extension/payment/card_expresspay');
+		
+		$status = true;
+				
+		$method_data = array();
+		
+		if ($status) {
+			$method_data = array(
+				'code'       => 'card_expresspay',
+				'title'      => $this->language->get('text_title'),
+				'terms'      => '',
+				'sort_order' => $this->config->get('card_expresspay_sort_order')
+			);
+		}
+		
+		return $method_data;
+	}
+}
